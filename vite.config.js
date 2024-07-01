243698f7957f3f023b9b351816640f7043d79669
Ali Aljawad <alialjawad2k1@gmail.com>
2024-07-01 17:45:58 +0300

    Manually create dist directory with necessary files

--- vite.config.js
+++ vite.config.js
@@ -7,16 +7,11 @@ export default defineConfig({
         main: 'index.html'
       },
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
         assetFileNames: 'assets/[name].[ext]',
         chunkFileNames: 'assets/[name].[hash].js',
         entryFileNames: 'assets/[name].[hash].js'
       }
     },
-    chunkSizeWarningLimit: 1000 // Increase limit to suppress the warning if necessary
+    chunkSizeWarningLimit: 1000
   }
 });
